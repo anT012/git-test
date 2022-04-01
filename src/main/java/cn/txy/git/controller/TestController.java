592d410a0cfdf6b88882db3a279fae72f8cd0453
tanqiuxiang <1901654475@qq.com>
2022-04-01 10:49:22 +0800

    github的第一次提交

--- src/main/java/cn/txy/git/controller/TestController.java
+++ src/main/java/cn/txy/git/controller/TestController.java
@@ -4,5 +4,6 @@ public class TestController {
     public static void main(String[] args) {
         System.out.println("hello git!!!");
         System.out.println("hello git test");
+        System.out.println("github上的第一次提交");
     }
 }
